dfa0922ac68749b497cea5c38ab7491c5e8bcdf5
yakult-lily-yu <lilyshow.10204@gmail.com>
2020-10-04 00:16:46 +0800

    first version

---
+++ hw1/HW1_E24066470.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) 
+{
+	cout<<"================================="<<endl;
+	cout<<"|        NAME: §Eªöç] ^ ^        |"<<endl;
+	cout<<"|        GENDER: male            |"<<endl;
+	cout<<"|        BIRTHDAY: 88/03/17      |"<<endl;
+	cout<<"|        PHONE: 0936756715       |"<<endl;
+	cout<<"| MAIL: lilyshow.10204@gmail.com |"<<endl;
+	cout<<"|                                |"<<endl;
+	cout<<"|      NICE TO MEET YOU > <      |"<<endl;
+	cout<<"================================="<<endl;
+	
+	system("pause"); 
+	    
+	return 0;
+}
